ce04e1c63d1e6546e26a5d5666ba872f2c10fb23
Nicola Occelli <nicola.occelli@bordet.be>
2023-06-16 14:15:17 +0200

    added multi label resnet

--- lightning_aracnn/model/components/resnet.py
+++ lightning_aracnn/model/components/resnet.py
@@ -1,5 +1,7 @@
 from torch import nn
 import torchvision
+from collections import OrderedDict
+import torch
 
 
 class ResNet(nn.Module):
@@ -35,3 +37,43 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+
+
+class MultiLabelResNet(nn.Module):
+    def __init__(
+        self,
+        resnet: torchvision.models.resnet.ResNet,
+        num_classes: int = 10,
+        freeze: bool = True,
+    ):
+        super().__init__()
+        self.backbone = resnet
+        self.in_features = self.backbone.fc.in_features
+
+        # remove classifier from network
+        self.backbone = nn.Sequential(
+            OrderedDict(list(self.backbone.named_children())[:-1])
+        )
+
+        # create one classifier head for each class
+        self.heads = nn.ModuleList(
+            [nn.Linear(self.in_features, 1) for _ in range(num_classes)]
+        )
+
+        # stop gradients for parameters in convolutional layers
+        if freeze:
+            self.freeze_backbone()
+
+    def freeze_backbone(self):
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        """
+        Features go through each classifier head to perform binary classification against all labels
+        """
+        features = self.backbone(x)
+        features = torch.flatten(features, 1)
+        logits = [head(features) for head in self.heads]
+
+        return logits
